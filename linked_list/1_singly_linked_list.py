3124175ee71afa84b7b57a98746ded0e5fd94cde
Alexander Kondratev <kondratev.av@inbox.ru>
2022-09-21 19:51:53 +0500

    finish linked list

--- linked_list/1_singly_linked_list.py
+++ linked_list/1_singly_linked_list.py
@@ -1,49 +1,57 @@
+from typing import Any
+
+
 class LinkedList:
     class Node:
-        def __init__(self, value):
+        def __init__(self, value: Any):
             self.value = value
             self.next_value = None
 
-        def __str__(self):
+        def __str__(self) -> str:
             return str(self.value)
 
     head: Node = None
 
-    def append(self, element) -> Node:
+    def append(self, element: Any) -> Node:
         """Adds an element to the end of the list."""
         element = self.Node(value=element)
+
         if not self.head:
             self.head = element
             return element
 
-        end = self.head
-        while end.next_value:
-            end = end.next_value
-        end.next_value = element
+        for node in self:
+            if not node.next_value:
+                node.next_value = element
         return element
 
-    def __len__(self):
-        length = 0
-        if not self.head:
-            return length
-
-        while self.head.next_value:
+    def __len__(self) -> int:
+        length: int = 0
+        for _ in self:
             length += 1
-            self.head = self.head.next_value
-        length += 1
         return length
 
-    def __iter__(self):
+    def __iter__(self) -> 'LinkedList':
         self.next = self.head
         return self
 
-    def __next__(self):
+    def __next__(self) -> Node:
         current = self.next
         if current:
             self.next = current.next_value
             return current
         else:
             raise StopIteration
+
+    def __str__(self) -> str:
+        string = '['
+        for index, element in enumerate(self):
+            if index == 0:
+                string += str(element)
+                continue
+            string += f', {element}'
+        string += ']'
+        return string
 
 
 def main() -> None:
@@ -52,8 +60,8 @@ def main() -> None:
     for element in range(10):
         linked_list.append(element)
 
-    for elements in linked_list:
-        print(elements)
+    print(len(linked_list))
+    print(linked_list)
 
 
 if __name__ == '__main__':
---
+++ linked_list/2_doubly_linked_list.py
@@ -0,0 +1,127 @@
+from typing import Any
+
+
+class LinkedList:
+    class Node:
+        def __init__(self, value: Any):
+            self.value = value
+            self.prev_value = None
+            self.next_value = None
+
+        def __str__(self) -> str:
+            return str(self.value)
+
+    __head: Node = None
+
+    def append(self, element: Any) -> Node:
+        """Adds an element to the end of the list."""
+        element = self.Node(value=element)
+
+        if not self.__head:
+            self.__head = element
+            return element
+
+        for node in self:
+            if not node.next_value:
+                node.next_value = element
+                element.prev_value = node
+
+        return element
+
+    def get(self, index: int) -> Node:
+        """Gets an element by its index."""
+        if not isinstance(index, int):
+            raise ValueError('index must be "int" type')
+        for idx, value in enumerate(self):
+            if idx == index:
+                return value
+            continue
+        raise IndexError('list index out of range')
+
+    def insert_after(self, index: int, value: Any) -> Node:
+        """Get element by index and inset value after."""
+        element = self.Node(value=value)
+        get_element = self.get(index=index)
+
+        element.next_value = get_element.next_value
+        element.prev_value = get_element
+        get_element.next_value = element
+
+        return element
+
+    def insert_before(self, index: int, value: Any) -> Node:
+        """Get element by index and inset value before."""
+        element = self.Node(value=value)
+        if index == 0:
+            get_element = self.get(index=index)
+            self.__head = element
+            get_element.prev_value = element
+            element.next_value = get_element
+            return element
+
+        get_element = self.get(index=index-1)
+        element.next_value = get_element.next_value
+        get_element.next_value = element
+        element.prev_value = get_element
+        return element
+
+    def __reversed__(self) -> 'LinkedList':
+        """Reverse list."""
+        prev = None
+        current = self.__head
+
+        while current:
+            next_ = current.next_value
+            current.next_value = prev
+            prev = current
+            current = next_
+        self.__head = prev
+        return self
+
+    def __len__(self) -> int:
+        """Return list length."""
+        length: int = 0
+        for _ in self:
+            length += 1
+        return length
+
+    def __iter__(self) -> 'LinkedList':
+        self.next = self.__head
+        return self
+
+    def __next__(self) -> Node:
+        current = self.next
+        if current:
+            self.next = current.next_value
+            return current
+        else:
+            raise StopIteration
+
+    def __str__(self) -> str:
+        string = '['
+        for index, element in enumerate(self):
+            if index == 0:
+                string += str(element)
+                continue
+            string += f', {element}'
+        string += ']'
+        return string
+
+
+def main() -> None:
+    linked_list = LinkedList()
+
+    for element in range(10):
+        linked_list.append(element)
+
+    linked_list.insert_after(index=0, value='insert_after')
+    linked_list.insert_before(index=0, value='insert_before')
+
+    print(linked_list)
+    print(len(linked_list))
+    print(reversed(linked_list))
+    print(linked_list.get(index=0))
+
+
+if __name__ == '__main__':
+    main()
